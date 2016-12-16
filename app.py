dc7c1a0616e24bcbd863a9a34d8f73b08c36965f
Simon Vadée <simon.vadee@gmail.com>
2016-12-16 14:58:56 +0100

    displaycy style

--- app.py
+++ app.py
@@ -1,12 +1,63 @@
 from os import environ
 
-from flask import Flask, render_template, url_for, jsonify, make_response
+from flask import Flask, render_template, url_for, jsonify, make_response, request, session
 
+from spacy import en
+
+class Engine:
+
+    def load(self):
+        self.nlp = en.English()
+
+    def parse_input(self, input):
+
+        def add_arc(token):
+            return {
+                "start": token.i if token.i < token.head.i else token.head.i,
+                "end": token.i if token.i > token.head.i else token.head.i,
+                "label": token.dep_,
+                "dir": "left" if token.i < token.head.i else "right"
+            }
+            
+        def add_word(token):
+            return {
+                "tag": token.tag_,
+                "pos": token.pos_,
+                "text": token.text,
+                "lemma": token.lemma_
+            }
+
+        doc = self.nlp(input)
+        ret = {"arcs": list(), "words": list()}
+        for token in doc:
+            new_arc = add_arc(token)
+            if new_arc["start"] != new_arc["end"]:
+                ret["arcs"].append(new_arc)
+            ret["words"].append(add_word(token))
+
+        return ret
+
+
+#### CREATE APPLICATION OBJECT
 app = Flask(__name__, template_folder='static/', static_url_path='', static_folder='static/')
 
-@app.route('/parse/<sentence>')
-def parse(sentence):
-    return make_response(jsonify("OK", 200))
+
+#### COMPILE SASS FOR UI
+# assets = Environment(app)
+# assets.url = app.static_url_path
+# scss = Bundle('css/style.sass', filters='pyscss', output='all.css')
+# assets.register('scss_all', scss)
+
+
+#### LOAD SPACY MODEL
+engine = Engine()
+engine.load()
+
+
+@app.route('/parse', methods=['POST'])
+def parse():
+    ret = engine.parse_input(request.form.get('input'))
+    return make_response(jsonify({"data": ret, "status": 200}))
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
--- static/nlp.js
+++ static/nlp.js
@@ -1,14 +1,21 @@
 
+$("#input").keyup(function(event){
+    if(event.keyCode == 13){
+    	process();
+    }
+});
+
 const displacy = new displaCy('http://localhost:4848', {
     container: '#displacy',
     format: 'spacy',
     distance: 300,
-    offsetX: 100
+    offsetX: 100,
+    bg: "#272822"
 });
 
 const parse = {
     arcs: [
-        { dir: 'right', end: 1, label: 'npadvmod', start: 0 }
+        { dir: 'left', end: 0, label: 'npadvmod', start: 1 }
     ],
     words: [
         { tag: 'UH', text: 'Hello' },
@@ -19,11 +26,17 @@ const parse = {
 console.log(displacy)
 
 process = function() {
-	$.get('/parse/'.concat("bonjour")).then( function() {
+	text_to_process = $("#input").val();
+	if (text_to_process !== undefined && text_to_process.length > 0) {
+		
+		$.post('/parse', {
+			"input": text_to_process
+		}).then(function(parsed) {
+			console.log(parsed.data)
+			displacy.render(parsed.data, {
+			    color: '#ff0000'
+			});
 
-		displacy.render(parse, {
-		    color: '#ff0000'
 		});
-
-	});
+	}
 }
