da0d9b52150a30e5ec02c6483398fab3bab35ed4
Simon Vadée <simon.vadee@gmail.com>
2016-12-16 16:43:45 +0100

    update

--- static/nlp.js
+++ static/nlp.js
@@ -8,8 +8,14 @@ $("#input").keyup(function(event){
 const displacy = new displaCy('http://localhost:4848', {
     container: '#displacy',
     format: 'spacy',
-    distance: 300,
-    offsetX: 100,
+	collapsePunct: true,
+    collapsePhrase: true,
+    distance: 200,
+    offsetX: 150,
+    arrowSpacing: 10,
+    arrowWidth: 8,
+    arrowStroke: 2,
+    wordSpacing: 40,
     bg: "#272822"
 });
 
@@ -25,6 +31,15 @@ const parse = {
 
 console.log(displacy)
 
+show_details = function(word, tokens) {
+	$.each(tokens, function(index, token) {
+		if (token.text == word) {
+			console.log(token)
+			$("#details").text(JSON.stringify(token, null, 2))
+		}
+	});
+}
+
 process = function() {
 	text_to_process = $("#input").val();
 	if (text_to_process !== undefined && text_to_process.length > 0) {
@@ -34,9 +49,14 @@ process = function() {
 		}).then(function(parsed) {
 			console.log(parsed.data)
 			displacy.render(parsed.data, {
-			    color: '#ff0000'
+			    arrowSpacing: 50
 			});
-
+			$(".displacy-token").click(function() {
+				show_details(this.children[0].textContent, parsed.data.words)
+			});
 		});
 	}
+
+
+
 }
